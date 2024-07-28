0763998ef7764673a9a1d1b74a8d45788ed4b0b3
Ng Chen Yang <chenyangng8@gmail.com>
2024-07-28 23:03:19 +0800

    Install tailwind and heroicon Done with the navigation bar Co-authored-by: Harithz <Arithz@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import Button from 'primevue/button';
 import Breadcrumb from 'primevue/breadcrumb';
+import Menubar from 'primevue/menubar';
+import './assets/main.css'
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
@@ -14,4 +16,5 @@ app.use(PrimeVue, {
 });
 app.component('Button', Button);
 app.component("Breadcrumb", Breadcrumb);
+app.component("Menubar", Menubar);
 app.mount('#app');
