926ac152d409c613633bcaaf178e88f9993342cc
rtolearn <chenyangng8@gmail.com>
2024-08-28 01:30:39 +0800

    hero section(after desing)

--- src/pages/homePage/IntroductionSection.vue
+++ src/pages/homePage/IntroductionSection.vue
@@ -1,58 +1,52 @@
 <template>
-  <div class="relative isolate overflow-hidden bg-white py-6">
-    <div
-      class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"
+  <!-- <div class="hero_section_background w-full h-[32rem] "></div> -->
+  <!-- <img
+      class="w-full h-auto  absolute opacity-20"
+      src="@/images/hero_section_pic.jpg"
+      alt=""
+    /> -->
+  <div class="relative z-10 w-4/5 h-auto py-[1rem] m-auto block">
+    <img
+      class="w-1/2 h-auto m-auto block"
+      src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
+      alt=""
     />
-    <div
-      class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"
-    />
-    <div class="mx-auto max-w-2xl lg:max-w-4xl m-0">
-      <img
-        class="mx-auto h-12"
-        src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
-        alt=""
-      />
-      <figure class="mt-10">
-        <blockquote
-          class="text-center text-md sm:text-xl font-semibold leading-8 text-gray-900 leading-8 pl-5 pr-5"
-        >
-          <p>
-            “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            expedita voluptas culpa sapiente alias molestiae. Numquam corrupti
-            in laborum sed rerum et corporis.”
-          </p>
-        </blockquote>
-      </figure>
-    </div>
+    <figure class="mt-1 sm:mt-5 text-center">
+      <h5
+        class="text-[0.65rem] sm:text-lg text-gray-400 leading-8 sm:leading-10 font-semibold font-['Poppins'] w-full h-auto sm:mb-[1rem]"
+      >
+        YOUR GENEROSITY CAN CHANGE LIVES FOREVER
+      </h5>
+      <h1
+        class="text-[1rem] sm:text-3xl leading-10 sm:leading-10 font-semibold font-['Poppins'] w-full h-auto mb-[1.25rem]"
+      >
+        Lend a Helping Hand to Those in Need
+      </h1>
+
+      <p
+        class="hidden sm:block text-xs sm:text-xl font-semibold leading-5 sm:leading-10 text-gray-900 leading-8 pl-5 pr-5 w-full h-auto m-auto block"
+      >
+        "Your kindness can provide shelter, education, and hope to children who
+        need it most. Join us in making a lasting difference, one donation at a
+        time"
+      </p>
+      <button
+        class="text-md border solid green rounded-[5px] w-3/5 h-auto bg-green-700 text-white my-[0.25rem] md:my-[1rem] lg:my-[3rem] font-['Poppins']"
+      >
+        Donate Now
+      </button>
+    </figure>
   </div>
-
-
-  <div class="w-4/5 sm:3/5 m-auto display-block ">
+  <div class="w-4/5 m-auto display-block py-[0.5rem]">
     <carousel :items-to-show="2" :autoplay="1000" :loop="true" :wrap-around="true" pause-autoplay-on-hover class="m-5">
       <slide v-for="image in data" :key="image.id" >
         <img :src="image.img" alt="Image" class="w-4/5 sm:w-2/5 h-auto"/>
       </slide>
-
-      <!-- <template #addons>
-        <div class="">
-           <navigation /> 
-           <pagination /> 
-        </div>
-      </template> -->
-
     </carousel>
   </div>
-
 </template>
 
-<script setup> 
-
-
-
-
-
-
-// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+<script setup>
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide} from "vue3-carousel";
 // import {Pagination, Navigation} from "vue3-carousel";
@@ -62,5 +56,15 @@ const data = [
   {img: "https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"},
   {img: "https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"},
   {img:  "https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"},
-]
-</script> 
+]</script>
+<style scoped>
+.hero_section_background {
+  background-image: url("@/images/hero_section_pic.jpg");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  opacity: 0.2;
+  position: absolute;
+  z-index: 1;
+}
+</style>
